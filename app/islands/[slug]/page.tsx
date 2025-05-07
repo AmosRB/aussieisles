d9f0f4af2d592186346ef158a7ae85b6d7bfa9db
AmosRB <96162009+AmosRB@users.noreply.github.com>
2025-05-07 20:51:42 +0300

    Update page.tsx הסיר לגמרי את IslandPageParams

--- app/islands/[slug]/page.tsx
+++ app/islands/[slug]/page.tsx
@@ -3,11 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { islands } from '../../../data/islands';
 
-interface IslandPageParams {
-  params: { slug: string };
-}
-
-export default async function IslandPage({ params }: IslandPageParams) {
+export default function IslandPage({ params }: { params: { slug: string } }) {
   const island = islands.find((i) => i.slug === params.slug);
 
   if (!island) {
@@ -16,6 +12,7 @@ export default async function IslandPage({ params }: IslandPageParams) {
 
   return (
     <main className="relative p-8 min-h-screen bg-gradient-to-b from-[oklch(95.6%_0.045_203.388)] to-[oklch(91%_0.096_180.426)] text-black">
+      {/* כפתור BACK בצד ימין למעלה */}
       <div className="absolute top-4 right-4">
         <Link
           href="/"
@@ -29,7 +26,7 @@ export default async function IslandPage({ params }: IslandPageParams) {
       <div className="flex">
         <Image src={island.image} alt={island.name} width={600} height={400} className="rounded-xl mb-4" />
         <div className="flex-1 flex items-center justify-center">
-          <div className="text-6xl font-extrabold text-red-800 drop-shadow-lg">{island.price}</div>
+          <div className="text-6xl font-extrabold text-red-800 drop-shadow-lg">{island.price} AUD</div>
         </div>
       </div>
       <p className="mb-8 text-lg">{island.description}</p>
