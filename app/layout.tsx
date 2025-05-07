2d6f73faf06899fa4058ae685f758e477db1314c
AmosRB <96162009+AmosRB@users.noreply.github.com>
2025-05-07 20:18:50 +0300

    Update layout.tsx ניקוי פונטים להעלאה

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,7 @@
-
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Aussie Isles",
-  description: "The place where islands come true",
-};
 
 export default function RootLayout({ children }) {
   return (
@@ -27,4 +10,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
